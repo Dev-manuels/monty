0c9178ea623bac42c34e5e9ae3ef67e10bd48b01
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-08-24 15:26:17 +0100

    add betty description for rotl function

--- rotl.c
+++ rotl.c
@@ -1,5 +1,9 @@
 #include "monty.h"
-
+/**
+ * rotl - rotates the stack to the top
+ * @stack: pointer to new node  to be pushed
+ * @line_number: line number of opcode in monty file
+*/
 void rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
